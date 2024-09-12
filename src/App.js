1db0a5b6d1bd5e68fcd4f7b3fa660fcd4daf9b4c
pnttiwari99@gmail.com <you@example.com>
2024-09-12 23:30:35 +0530

    bckend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+const App = () => {
+    const [todos, setTodos] = useState([]);
+
+    // Fetch todos
+    useEffect(() => {
+        axios.get('http://localhost:5000/todos')
+            .then(response => setTodos(response.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    const addTodo = (text) => {
+        axios.post('http://localhost:5000/todos', { text })
+            .then(response => setTodos([...todos, response.data]))
+            .catch(err => console.log(err));
+    };
+
+    const deleteTodo = (id) => {
+        axios.delete(`http://localhost:5000/todos/${id}`)
+            .then(() => setTodos(todos.filter(todo => todo._id !== id)))
+            .catch(err => console.log(err));
+    };
+
+    return (
+        <div>
+            <h1>Todo App</h1>
+            <TodoForm addTodo={addTodo} />
+            <TodoList todos={todos} deleteTodo={deleteTodo} />
+        </div>
+    );
+};
 
 export default App;
